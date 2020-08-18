c0f0ceb8404e4898a2cb520fc20bcafa7c9e760e
Lloyd Leagas <lloyd.leagas@kurtosys.com>
2020-08-18 09:12:59 +0200

    Update database table structure

--- db/artifacts/init.sql
+++ db/artifacts/init.sql
@@ -4,13 +4,31 @@ CREATE DATABASE card_db;
 
 USE card_db;
 
+CREATE TABLE Topic (
+	topicId int(11) NOT NULL AUTO_INCREMENT,
+	topic VARCHAR(255),
+	PRIMARY KEY (topicId)
+);
+
+CREATE TABLE Image (
+	imageId int(11) NOT NULL AUTO_INCREMENT,
+	image BLOB,
+	PRIMARY KEY (imageId)
+);
+
 CREATE TABLE Card (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  topic VARCHAR(255),
+  cardId int(11) NOT NULL AUTO_INCREMENT,
+  topicId int(11),
+	imageId int(11),
   question VARCHAR(255),
   answer VARCHAR(255),
 	known int(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (id)
+	FOREIGN KEY (topicId)
+		REFERENCES Topic(topicId),
+	FOREIGN KEY (imageId)
+		REFERENCES Image(imageId),
+  PRIMARY KEY (cardId)
 );
 
-INSERT INTO Card  (id, topic, question, answer) VALUES (0, 'Default', 'Question', 'Answer');
+INSERT INTO Topic (topic) VALUES ('Default');
+INSERT INTO Card  (topicId, question, answer) VALUES (1, 'Question', 'Answer');
